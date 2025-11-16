8eb0aece5e6d6cf5f6c4c64838ad7a40c1ad0f0d
Juan <Juan@192.168.3.100>
2025-11-15 23:37:02 -0500

    primer commit

---
+++ HolaMundo/src/Paquete/HolaMundo.java
@@ -0,0 +1,12 @@
+package Paquete;
+
+public class HolaMundo {
+
+	public static void main (String args[]) {
+		System.out.println("Hola Mundo");
+		// Otro metodo para imprimir es declarando las variables
+		String mensaje = "GRACIAS TOTALES";
+		System.out.println(mensaje);
+	}
+}
+ 
---
+++ HolaMundo/src/Paquete/Variables.java
@@ -0,0 +1,13 @@
+package Paquete;
+
+public class Variables {
+//Byte: Emplea un sólo byte (8 bits) de almacenamiento
+//Esto permite almacenar valores entre [-128, 127].
+	
+	byte numeroByte = 9;
+	
+//short: Emplea el doble de almacenamiento de (byte)
+//Esto permite almacenar valores entre [-32.768, 32.767].
+
+
+}
