82761b4b0bb9e896fe7e20f629708aa5b83a72b9
Juan <Juan@192.168.3.100>
2025-11-16 18:46:30 -0500

    Soluccionado ejercicios de variables

---
+++ HolaMundo/src/ClasesVariables/Ejercicio1.java
@@ -0,0 +1,19 @@
+package ClasesVariables;
+
+public class Ejercicio1 {
+
+	public static void main(String args[]) {
+		
+	String nombre = "Diego Alberto Yepes Hugo";
+	double x = 2.54;
+	final double dolar = 3.764;
+	double valorPI = 3.1415926F;
+	double valorE = 2.718281828459045;
+	
+	System.out.println("La variable del nombre es: " + nombre);
+	System.out.println("La variable de x es: " + x);
+	System.out.println("La variable del dolar es: " + dolar);
+	System.out.println("La variable del valor de pi es: " + valorPI );
+	System.out.println("La variable del valor de euler es: " + valorE);
+	}
+}
---
+++ HolaMundo/src/ClasesVariables/Ejercicio2.java
@@ -0,0 +1,15 @@
+package ClasesVariables;
+
+public class Ejercicio2 {
+
+	public static void main(String args[]) {
+		
+		int a =8;
+		char b = 'A';
+		String c = "Colombia";
+		
+		
+		
+	}
+	
+}
