c213f5f30ee493c54fa2773ad8d16d6d5fac1fb0
Juan <Juan@192.168.3.100>
2025-11-16 19:15:32 -0500

    agregadas las 7 formas de las matrizes

--- HolaMundo/src/Paquete/Vectores_matriz.java
+++ HolaMundo/src/Paquete/Vectores_matriz.java
@@ -1,5 +1,22 @@
 package Paquete;
 
 public class Vectores_matriz {
-
+	// Matriz: Se emplea para almacenar un grupo de datos del mismo
+	// de forma bidimensional basados en [x],[y]
+	
+	//Forma 1:
+	int matrizNumeros1[][] = new int [4][5];
+	//Forma 2:
+	int [][] matrizNumeros2 = new int [4][5];
+	//Forma 3:
+	int[][] matrizNumeros3 = new int [4][5];
+	//Forma 4:
+	int matrizNumeros4[][];
+	//Forma 5:
+	int matrizNumeros5[][] = {};	
+	//Forma 6:
+	int matrizNumeros6[][] = {{1,2} , {3,9}};
+	//Forma 7:
+	int matrizNumeros7[][] = new int[][] {{6,2},{2,7}};
+	
 }
