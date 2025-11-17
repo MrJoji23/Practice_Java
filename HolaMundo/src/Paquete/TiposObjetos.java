bd1b093dbab06b61d09dfabb277bbb1b1fd23886
Juan <Juan@192.168.3.100>
2025-11-17 16:53:55 -0500

    pagina 29 guia1

---
+++ HolaMundo/src/Paquete/TiposObjetos.java
@@ -0,0 +1,33 @@
+package Paquete;
+
+public class TiposObjetos {
+	
+	//Estos tipos son equivalentes a los primitivos pero en forma
+	//de objetos son: Byte, Short, Integer, Long, Float, Double, 
+	//Boolean y Character (8 igualmente)
+	
+	//Representación de byte en Byte.
+	Byte numeroByte = 1;
+
+	//Representación de short en Short.
+	Short numeroShort = 2416;
+
+	//Representación de int en Integer.
+	Integer numeroInteger = 95256712;
+
+	//Representación de long en Long.
+	Long numeroLong = 52137141211L;
+
+	//Representación de float en Float.
+	Float numeroFloat = 6591324543251F;
+
+	//Representación de double en Double.
+	Double numeroDouble = 9.3;
+
+	//Representación de boolean en Boolean.
+	Boolean variableBoolean = true;
+
+	//Representación de char en Character.
+	Character variableCharacter = 'A';
+	Character numeroCharacter = 2;
+}
---
+++ HolaMundo/src/Paquete/constantes.java
@@ -0,0 +1,22 @@
+package Paquete;
+
+public class constantes {
+
+	public static void main (String args[]) {
+		// Java ha reservado la palabra "final" para definir constantes.
+		//En Java es muy simple definir constantes, solo basta con agregar el
+		// modificador "final" antes de la declaración del tipo
+		
+		final int documento = 921474159;
+		final char letraA = 'A', letraB = 'B', letraC = 'C';
+		final String acronimo = "CC";
+		final float pi;
+		pi = 3.1415F;
+		
+		//Se pueden declarar N cantidad de constantes, siempre y cuando sean necesarias.
+		//SIEMPRE debe estar presente la palabra final.
+		//Se puede o no inicializar la constante en su creación. 
+		//El valor que se asigne no puede ser modificado en ejecución. 
+		
+	}
+}
