4635fad5e1dd32d4d646f813d0b0f1e028a54d84
Juan <Juan@192.168.3.100>
2025-11-17 20:23:14 -0500

    mrj

--- HolaMundo/src/Paquete/TiposEsctructurados.java
+++ HolaMundo/src/Paquete/TiposEsctructurados.java
@@ -3,19 +3,19 @@ package Paquete;
 public class TiposEsctructurados {
 
 	public static void main (String args[]) {
-	// Variable de tipo persona (Persona es una clase);
-		Persona P;
+
+		// Variable de tipo persona (Persona es una clase);
+		//Persona P;
 	// Variable de tipo animal (animal es una clase);
-		Animal A;
+		//Animal A;
 	//Variable de ajava de clase Math
 		Math M;
 	//Variable de la clase Scanner.
-		Scanner S;
+		//Scanner S;
 	//Variable de Java de clase BufferedReader
-		BufferedReader B;
+		//BufferedReader B;
 		
 		
-	
 		
 	}
 	
