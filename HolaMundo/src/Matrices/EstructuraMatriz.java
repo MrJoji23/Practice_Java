c0f779e9fa0034cf1af711e2e6f21c9207b74472
MrJoji23 <juaanmaarin23@outlook.com>
2025-11-25 13:01:09 -0500

    Se inicia Matrices, faltando ejercicios y estructura

---
+++ HolaMundo/src/Matrices/EstructuraMatriz.java
@@ -0,0 +1,9 @@
+package Matrices;
+
+public class EstructuraMatriz {
+
+	//Los elementos de una matriz o array bidimensional se empiezan a numerar 
+	//en el 0,0 (haciendo referencia a las filas y las columnas) y permiten gestionar 
+	//desde una sola variable múltiples datos del mismo tipo.
+	
+}
---
+++ HolaMundo/src/Matrices/Matrices.java
@@ -0,0 +1,20 @@
+package Matrices;
+
+public class Matrices {
+
+	//Forma 1:
+	int matrizNumeros1[][] = new int[4][5];
+	//Forma 2:
+	int [][]matrizNumeros2 = new int[4][5];
+	//Forma 3:
+	int[] [] matrizNumeros3 = new int[4][5];
+	//Forma 4:
+	int matrizNumeros4[][];
+	//Forma 5:
+	int matrizNumeros5[][] = {};
+	//Forma 6:
+	int matrizNumeros6[][] = {{1,2},{3,9}};
+	//Forma 7:
+	int matrizNumeros7[][] = new int[][]{{6,2},{2,7}};
+
+}
